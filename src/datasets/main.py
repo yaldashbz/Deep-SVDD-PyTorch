36cd237150edc72b4c90cecbf39ea579da0e99a3
yaldashbz <yaldashabanzadeh@gmail.com>
2022-03-02 16:11:24 +0330

    feat: add tinyimgnet dataset

--- src/datasets/main.py
+++ src/datasets/main.py
@@ -1,11 +1,12 @@
 from .mnist import MNIST_Dataset
 from .cifar10 import CIFAR10_Dataset
+from .tinyimgnet import TinyImgNet_Dataset
 
 
 def load_dataset(dataset_name, data_path, normal_class):
     """Loads the dataset."""
 
-    implemented_datasets = ('mnist', 'cifar10')
+    implemented_datasets = ('mnist', 'cifar10', 'tinyimgnet')
     assert dataset_name in implemented_datasets
 
     dataset = None
@@ -15,5 +16,8 @@ def load_dataset(dataset_name, data_path, normal_class):
 
     if dataset_name == 'cifar10':
         dataset = CIFAR10_Dataset(root=data_path, normal_class=normal_class)
+
+    if dataset_name == 'tinyimgnet':
+        dataset = TinyImgNet_Dataset(root=data_path, normal_class=normal_class)
 
     return dataset
--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from datasets.main import load_dataset
 # Settings
 ################################################################################
 @click.command()
-@click.argument('dataset_name', type=click.Choice(['mnist', 'cifar10']))
+@click.argument('dataset_name', type=click.Choice(['mnist', 'cifar10', 'tinyimgnet']))
 @click.argument('net_name', type=click.Choice(['mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU']))
 @click.argument('xp_path', type=click.Path(exists=True))
 @click.argument('data_path', type=click.Path(exists=True))
@@ -169,13 +169,13 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
     indices, labels, scores = np.array(indices), np.array(labels), np.array(scores)
     idx_sorted = indices[labels == 0][np.argsort(scores[labels == 0])]  # sorted from lowest to highest anomaly score
 
-    if dataset_name in ('mnist', 'cifar10'):
+    if dataset_name in ('mnist', 'cifar10', 'tinyimgnet'):
 
         if dataset_name == 'mnist':
             X_normals = dataset.test_set.test_data[idx_sorted[:32], ...].unsqueeze(1)
             X_outliers = dataset.test_set.test_data[idx_sorted[-32:], ...].unsqueeze(1)
 
-        if dataset_name == 'cifar10':
+        else:
             X_normals = torch.tensor(np.transpose(dataset.test_set.test_data[idx_sorted[:32], ...], (0, 3, 1, 2)))
             X_outliers = torch.tensor(np.transpose(dataset.test_set.test_data[idx_sorted[-32:], ...], (0, 3, 1, 2)))
 
